b4fbac9c42f02f84ee3438749afbd349de9527fe
JorgeMR5 <144338977+JorgeMR5@users.noreply.github.com>
2026-01-08 11:42:16 +0100

    Primera subida

---
+++ RPMI2D_FLO/Assets/_RPMI2D_Root/Scripts/NegativeCoin.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NegativeCoin : MonoBehaviour
+{
+    [SerializeField] int minusHealth;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Se accede a la vida en el GameManager, se resta y luego se apaga la moneda
+            GameManager.Instance.playerHealth -= minusHealth;
+            gameObject.SetActive(false);
+        }
+    }
+}
